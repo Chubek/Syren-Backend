134f1efed4b7854dd710e2acbb371b249fc988fe
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-07-13 07:14:43 +0430

    login and register done

--- src/main/kotlin/com/syren/backend/syrenbackend/dto/mapper/DtoMappers.kt
+++ src/main/kotlin/com/syren/backend/syrenbackend/dto/mapper/DtoMappers.kt
@@ -9,57 +9,91 @@ import com.syren.backend.syrenbackend.model.resource.*
 import com.syren.backend.syrenbackend.model.stream.OverrideMessage
 import com.syren.backend.syrenbackend.model.stream.OverrideResource
 import com.syren.backend.syrenbackend.model.stream.Stream
-import com.syren.backend.syrenbackend.model.stream.StreamLayoutTemplate
 import org.modelmapper.ModelMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.stereotype.Component
 
-fun monitorDtoMapper(monitor: Monitor) {
-    return ModelMapper().map(monitor, MonitorDto::class)
-}
+@Component
+@ComponentScan
+class DtoMappers {
 
-fun playListDtoMapper(playList: PlayList) {
-    return ModelMapper().map(playList, PlayListDto::class)
-}
+    @Autowired
+    private lateinit var modelMapper: ModelMapper
 
-fun scheduleDtoMapper(schedule: Schedule) {
-    return ModelMapper().map(schedule, ScheduleDto::class)
-}
+    fun moderatorMapperDto(moderator: Moderator): ModeratorDto {
+        return modelMapper.map(moderator, ModeratorDto::class.java)
+    }
+    fun audioResourceMapperDto(audioResource: AudioResource): AudioResourceDto {
+        return modelMapper.map(audioResource, AudioResourceDto::class.java)
+    }
+    fun imageResourceMapperDto(imageResource: ImageResource): ImageResourceDto {
+        return modelMapper.map(imageResource, ImageResourceDto::class.java)
+    }
+    fun monitorMapperDto(monitor: Monitor): MonitorDto {
+        return modelMapper.map(monitor, MonitorDto::class.java)
+    }
+    fun overrideMessageMapperDto(overrideMessage: OverrideMessage): OverrideMessageDto {
+        return modelMapper.map(overrideMessage, OverrideMessageDto::class.java)
+    }
+    fun overrideResourceMapperDto(overrideResource: OverrideResource): OverrideResourceDto {
+        return modelMapper.map(overrideResource, OverrideResourceDto::class.java)
+    }
+    fun playListMapperDto(playList: PlayList): PlayListDto {
+        return modelMapper.map(playList, PlayListDto::class.java)
+    }
+    fun scheduleMapperDto(schedule: Schedule): ScheduleDto {
+        return modelMapper.map(schedule, ScheduleDto::class.java)
+    }
+    fun streamMapperDto(stream: Stream): StreamDto {
+        return modelMapper.map(stream, StreamDto::class.java)
+    }
+    fun videoResourceMapperDto(videoResource: VideoResource): VideoResourceDto {
+        return modelMapper.map(videoResource, VideoResourceDto::class.java)
+    }
+    fun webPageResourceMapperDto(webPageResource: WebPageResource): WebPageResourceDto {
+        return modelMapper.map(webPageResource, WebPageResourceDto::class.java)
+    }
+    fun widgetResourceMapperDto(widgetResource: WidgetResource): WidgetResourceDto {
+        return modelMapper.map(widgetResource, WidgetResourceDto::class.java)
+    }
 
-fun moderatorDtoMapper(moderator: Moderator) {
-    return ModelMapper().map(moderator, ModeratorDto::class)
-}
+    fun audioResourceMapperEntity(audioResourceDto: AudioResourceDto): AudioResource {
+        return modelMapper.map(audioResourceDto, AudioResource::class.java)
+    }
+    fun imageResourceMapperEntity(imageResourceDto: ImageResourceDto): ImageResource {
+        return modelMapper.map(imageResourceDto, ImageResource::class.java)
+    }
+    fun monitorMapperEntity(monitorDto: MonitorDto): Monitor {
+        return modelMapper.map(monitorDto, Monitor::class.java)
+    }
+    fun moderatorMapperEntity(moderatorDto: ModeratorDto): Moderator {
+        return modelMapper.map(moderatorDto, Moderator::class.java)
+    }
+    fun overrideMessageMapperEntity(overrideMessageDto: OverrideMessageDto): OverrideMessage {
+        return modelMapper.map(overrideMessageDto, OverrideMessage::class.java)
+    }
+    fun overrideResourceMapperEntity(overrideResourceDto: OverrideResourceDto): OverrideResource {
+        return modelMapper.map(overrideResourceDto, OverrideResource::class.java)
+    }
+    fun playListMapperEntity(playListDto: PlayListDto): PlayList {
+        return modelMapper.map(playListDto, PlayList::class.java)
+    }
+    fun scheduleMapperEntity(scheduleDto: ScheduleDto): Schedule {
+        return modelMapper.map(scheduleDto, Schedule::class.java)
+    }
+    fun streamMapperEntity(streamDto: StreamDto): Stream {
+        return modelMapper.map(streamDto, Stream::class.java)
+    }
+    fun videoResourceMapperEntity(videoResourceDto: VideoResourceDto): VideoResource {
+        return modelMapper.map(videoResourceDto, VideoResource::class.java)
+    }
+    fun webPageResourceMapperEntity(webPageResourceDto: WebPageResourceDto): WebPageResource {
+        return modelMapper.map(webPageResourceDto, WebPageResource::class.java)
+    }
+    fun widgetResourceMapperEntity(widgetResourceDto: WidgetResourceDto): WidgetResource {
+        return modelMapper.map(widgetResourceDto, WidgetResource::class.java)
+    }
 
-fun audioResourceDtoMapper(audioResource: AudioResource) {
-    return ModelMapper().map(audioResource, AudioResourceDto::class)
-}
 
-fun imageResourceDtoMapper(imageResource: ImageResource) {
-    return ModelMapper().map(imageResource, ImageResourceDto::class)
-}
-
-fun videoResourceDtoMapper(videoResource: VideoResource) {
-    return ModelMapper().map(videoResource, VideoResourceDto::class)
-}
-
-fun webPageResourceDtoMapper(webPageResource: WebPageResource) {
-    return ModelMapper().map(webPageResource, WebPageResourceDto::class)
-}
-
-fun widgetResourceDtoMapper(widgetResource: WidgetResource) {
-    return ModelMapper().map(widgetResource, WidgetResourceDto::class)
-}
-
-fun overrideMessageDtoMapper(overrideMessage: OverrideMessage) {
-    return ModelMapper().map(overrideMessage, OverrideMessageDto::class)
-}
-
-fun overrideResourceDtoMapper(overrideResource: OverrideResource) {
-    return ModelMapper().map(overrideResource, OverrideResourceDto::class)
-}
-
-fun streamDtoMapper(stream: Stream) {
-    return ModelMapper().map(stream, StreamDto::class)
-}
-
-fun streamLayoutTemplateMapper(layoutTemplate: StreamLayoutTemplate) {
-    return ModelMapper().map(layoutTemplate, StreamLayoutTemplateDto::class)
 }
--- src/main/kotlin/com/syren/backend/syrenbackend/service/moderator/ModeratorService.kt
+++ src/main/kotlin/com/syren/backend/syrenbackend/service/moderator/ModeratorService.kt
@@ -1,6 +1,8 @@
 package com.syren.backend.syrenbackend.service.moderator
 
+import com.mongodb.internal.connection.tlschannel.util.Util.assertTrue
 import com.syren.backend.syrenbackend.dao.impl.ModeratorDao
+import com.syren.backend.syrenbackend.dto.mapper.DtoMappers
 import com.syren.backend.syrenbackend.dto.modeldataclass.ModeratorDto
 import com.syren.backend.syrenbackend.model.moderator.Moderator
 import com.syren.backend.syrenbackend.service.security.JWT
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
+import java.lang.Exception
 
 @Service
 @ComponentScan
@@ -24,17 +27,54 @@ class ModeratorService {
     @Autowired
     private lateinit var passwordEncoder: PasswordEncoder
 
+    @Autowired
+    private lateinit var dtoMappers: DtoMappers
+
+    val emailRegex = """(?:[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#${'$'}%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""".toRegex()
+    val mobileNumberRegex = """^(00|\+)[1-9]{1}([0-9][\s]*){9,16}${'$'}""".toRegex()
+
     fun registerModerator(email: String, password: String, firstName: String, lastName: String, mobileNumber: String): String {
+
+        try {
+            assertTrue(emailRegex matches  email)
+            assertTrue(mobileNumberRegex matches  mobileNumber)
+        } catch (e: Exception) {
+            throw e
+        }
+
 
         val hash = passwordEncoder.encode(password).toString()
 
         val moderator = Moderator(email = email, firstName = firstName, lastName = lastName, password = hash, mobileNumber = mobileNumber)
 
-        return jwt.createJwt(moderator.id)
+        val moderatorDto = dtoMappers.moderatorMapper(moderator)
+
+        return moderatorDao.createModerator(moderatorDto)
     }
 
+    fun authModerator(loginString: String, password: String): Moderator {
 
+        lateinit var moderatorDto: ModeratorDto
 
+       when {
+           emailRegex matches loginString -> {
+                moderatorDto = moderatorDao.findByEmail(loginString)
+           }
+           mobileNumberRegex matches  loginString -> {
+               moderatorDto = moderatorDao.findByMobileNumber(loginString)
+           }
+       }
+
+        val hash = passwordEncoder.encode(password)
+
+        try {
+            assertTrue(hash == moderatorDto.password)
+        } catch (e: Exception) {
+            throw e
+        }
+
+        return dtoMappers.moderatorMapperEntity(moderatorDto)
+    }
 
 }
 
