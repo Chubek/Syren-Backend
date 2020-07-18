af15c6e09c8c265496670ac64df319056dda7ffe
Chubak Bidpaa <chubakbidpaa@gmail.com>
2020-07-18 12:45:20 +0430

    added first controller

--- src/main/kotlin/com/syren/backend/syrenbackend/controller/resource/MediaResourceController.kt
+++ src/main/kotlin/com/syren/backend/syrenbackend/controller/resource/MediaResourceController.kt
@@ -1,9 +1,13 @@
 package com.syren.backend.syrenbackend.controller.resource
 
+import com.syren.backend.syrenbackend.service.file.uploadToStorage
 import com.syren.backend.syrenbackend.service.resource.MediaResourceService
+import com.syren.backend.syrenbackend.subschema.requestobjects.BucketName
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 
 @RestController
@@ -12,6 +16,16 @@ class MediaResourceController {
 
     @Autowired
     private lateinit var mediaResourceService: MediaResourceService
+
+    @PostMapping("/upload/media")
+    @ResponseBody
+    fun uploadFileToCloud(@RequestParam("file") file: MultipartFile, @RequestBody bucketName: BucketName): ResponseEntity<String> {
+        uploadToStorage(file, bucketName.bucketName)
+
+        return ResponseEntity(HttpStatus.OK)
+    }
+
+
 
 
 
---
+++ src/main/kotlin/com/syren/backend/syrenbackend/subschema/requestobjects/RequestBodies.kt
@@ -0,0 +1,3 @@
+package com.syren.backend.syrenbackend.subschema.requestobjects
+
+data class BucketName(val bucketName: String)
